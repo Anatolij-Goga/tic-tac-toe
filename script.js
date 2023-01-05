ef03eb29bb69f34c8fe34809e4d44ff6189f918a
Anatolij Goga <anzefal@gmail.com>
2023-01-05 16:22:30 +0200

    Added LocalStorage

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const content = document.querySelector('.content');
-const victories = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-  [1, 4, 7],
-  [2, 5, 8],
-  [3, 6, 9],
-  [1, 5, 9],
-  [3, 5, 7],
-];
-const moveX = [];
-const moveO = [];
 
-let player = 'X';
+const KEY_X = 'keyX';
+const KEY_O = 'keyO';
+const KEY_PLAYER = 'player';
+const KEY_COUNTER = 'counter';
+const endGame = 9;
+let counter = JSON.parse(localStorage.getItem(KEY_COUNTER)) || 0;
+let player = localStorage.getItem(KEY_PLAYER) || 'X';
+
+const moveX = JSON.parse(localStorage.getItem(KEY_X)) || [];
+const moveO = JSON.parse(localStorage.getItem(KEY_O)) || [];
+
+function autoComplite() {
+  [...content.children].forEach(item => {
+    const id = Number(item.dataset.id);
+    if (moveX.includes(id)) {
+      item.textContent = 'X';
+    } else if (moveO.includes(id)) {
+      item.textContent = 'O';
+    }
+  });
+}
 
 function creatMarkUp() {
   let markUp = '';
@@ -25,6 +33,7 @@ function creatMarkUp() {
 }
 
 creatMarkUp();
+autoComplite();
 
 content.addEventListener('click', onClick);
 
@@ -35,19 +44,20 @@ function onClick(event) {
   if (event.target.textContent) {
     return;
   }
-  // if (event.target === event.currentTarget) {
-  //   return;
-  // }
 
   const currentId = Number(event.target.dataset.id);
 
   let result = false;
 
+  counter += 1;
+
   if (player === 'X') {
     moveX.push(currentId);
+    localStorage.setItem(KEY_X, JSON.stringify(moveX));
     result = isVictorious(moveX);
   } else {
     moveO.push(currentId);
+    localStorage.setItem(KEY_O, JSON.stringify(moveO));
     result = isVictorious(moveO);
   }
 
@@ -58,15 +68,34 @@ function onClick(event) {
     return;
   }
 
+  if (counter === endGame) {
+    winner('RESTART GAME!!!');
+    return;
+  }
+
   player = player === 'X' ? 'O' : 'X';
+  localStorage.setItem(KEY_PLAYER, player);
+  localStorage.setItem(KEY_COUNTER, JSON.stringify(counter));
 }
 
 function winner(player) {
   setTimeout(() => {
     alert(player);
     creatMarkUp();
+    localStorage.clear();
   }, 500);
 }
+
+const victories = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
 
 function isVictorious(array) {
   return victories.some(item => item.every(id => array.includes(id)));
